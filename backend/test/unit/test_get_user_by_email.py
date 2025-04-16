ceb0954b70b6a9e784e43bd4da7c0b15fdf05520
teophilsimao <teophilsimao@gmail.com>
2025-04-16 15:00:09 +0200

    test get_user_by_email

--- backend/test/unit/test_get_user_by_email.py
+++ backend/test/unit/test_get_user_by_email.py
@@ -4,7 +4,7 @@ from src.controllers.usercontroller import UserController
 from src.util.dao import DAO
 
 @pytest.fixture
-def scu():
+def sut():
     mocked_dao = mock.MagicMock(spec=DAO)
     controller = UserController(dao=mocked_dao)
     return controller, mocked_dao
@@ -18,8 +18,8 @@ def scu():
         ("no@test.com", [], None, None, None),
     ]
 )
-def test_get_user_by_email(scu, email, dao_return, expected_output, expect_expection, warning_message, capsys):
-    controller, mocked_dao = scu
+def test_get_user_by_email(sut, email, dao_return, expected_output, expect_expection, warning_message, capsys):
+    controller, mocked_dao = sut
     mocked_dao.find.return_value = dao_return
     result = controller.get_user_by_email(email=email)
     assert result == expected_output
