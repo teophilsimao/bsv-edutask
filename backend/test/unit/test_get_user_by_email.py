85748b6cc1bf990c6e866c53e71358ab9283b864
teophilsimao <teophilsimao@gmail.com>
2025-04-17 13:35:45 +0200

    hej

--- backend/test/unit/test_get_user_by_email.py
+++ backend/test/unit/test_get_user_by_email.py
@@ -11,16 +11,39 @@ def sut():
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "email, dao_return, expected_output, expect_expection, warning_message",
+    "email, dao_return, expected_output, expect_expection",
     [
+        # Valid Email
         # Found user
-        ("user@test.com", [{"_id": 1, "email": "user@test.com"}], {"_id": 1, "email": "user@test.com"}, None, None),
+        ("user@test.com", [{"_id": 1, "email": "user@test.com"}], {"_id": 1, "email": "user@test.com"}, None),
+        ("many@test.com", [{"_id": 1, "email": "many@test.com"}, {"_id": 2, "email": "many@test.com"}], {"_id": 1, "email": "many@test.com"}, None),
         # No user
-        ("no@test.com", [], None, None, None),
+        ("no@test.com", [], None, None),
+
+        # Invalid Email
+        # ("user.com", None, None, ValueError),
+        # ("test@test", None, None, ValueError),
+        # ("test@", None, None, ValueError),
+        # ("test @test.com", None, None, ValueError),
+        # ("@test.com", None, None, ValueError),
+        # ("test", None, None, ValueError),
+        # ("@test", None, None, ValueError),
+        # ("@", None, None, ValueError),
     ]
 )
-def test_get_user_by_email(sut, email, dao_return, expected_output, expect_expection, warning_message):
+def test_get_user_by_email(sut, email, dao_return, expected_output, expect_expection):
     controller, mocked_dao = sut
-    mocked_dao.find.return_value = dao_return
-    result = controller.get_user_by_email(email=email)
-    assert result == expected_output
+    if expect_expection:
+        with pytest.raises(expect_expection):
+            controller.get_user_by_email(email=email)
+    else:
+        mocked_dao.find.return_value = dao_return
+        result = controller.get_user_by_email(email=email)
+        assert result == expected_output
+
+@pytest.mark.unit
+def test_get_user_by_email_error(sut):
+    controller, mocked_dao = sut
+    mocked_dao.find.side_effect = Exception
+    with pytest.raises(Exception):
+        controller.get_user_by_email('user@test.com')
